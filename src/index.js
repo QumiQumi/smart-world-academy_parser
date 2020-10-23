e67d436d7e90daa338c781327d8de27bce20453e
QumiQumi <glebyan1998@gmail.com>
2020-10-24 01:18:16 +0700

    обработаны маски для инпутов

--- src/index.js
+++ src/index.js
@@ -9,11 +9,14 @@ import signup from "./json/signup.js";
 
 var fileNames = getFileNames();
 var files = getFiles();
-// console.log(fileNames);
+console.log(fileNames);
 // console.log(files);
 
 fillDropdown();
-
+///////////////////
+makeFields(files.get("interview").fields);
+addKeypressMaskListeners();
+////////////////////////
 $("#input-form").submit((event) => {
 	event.preventDefault();
 	var select = $("#select");
@@ -21,6 +24,7 @@ $("#input-form").submit((event) => {
 	console.log(file);
 	makeButtons(file.buttons);
 	makeFields(file.fields);
+	addKeypressMaskListeners();
 });
 
 //Парсит поле buttons
@@ -39,16 +43,21 @@ function makeFields(fieldsArray) {
 	fieldsForm.empty();
 	if (fieldsArray == null) return;
 
-	fieldsArray.forEach((field) => {
+	fieldsArray.forEach((field, i) => {
 		$("<div>")
 			.attr({ class: "fields__line" })
-			.append($("<label/>").text(field.label))
+			.append(
+				$("<label/>")
+					.text(field.label)
+					.attr({ for: "input-" + i })
+			)
+
 			.append(() => {
 				var needCheckboxes = false;
 				var checkboxArray = [];
 
 				for (var key in field.input) {
-					if (Array.isArray(field.input[key])) {
+					if (Array.isArray(field.input[key]) && key != "filetype") {
 						needCheckboxes = true;
 						checkboxArray = field.input[key];
 					}
@@ -69,7 +78,7 @@ function makeFields(fieldsArray) {
 								.append(
 									$("<input/>").attr({
 										type: "checkbox",
-										name: "checkbox",
+										name: "checkbox" + i,
 										id: element,
 									})
 								)
@@ -89,13 +98,31 @@ function makeFields(fieldsArray) {
 					});
 					return checkboxes;
 				} else {
+					var filetypes =
+						field.input.filetype != null
+							? field.input.filetype.map((i) => "." + i)
+							: "";
+					var inputType =
+						field.input.mask != null ? "text" : field.input.type;
+					var maxLength =
+						field.input.mask != null ? field.input.mask.length : "";
+					var placeholder;
+					if (
+						field.input.placeholder == null &&
+						field.input.mask != null
+					)
+						placeholder = field.input.mask;
+					else placeholder = field.input.placeholder;
 					return $("<input/>").attr({
-						type: field.input.type,
+						type: inputType,
 						required: field.input.required,
-						placeholder: field.input.placeholder,
+						placeholder: placeholder,
 						multiple: field.input.multiple,
 						mask: field.input.mask,
-						filetype: field.input.filetype,
+						accept: filetypes,
+						id: "input-" + i,
+						maxlength: maxLength,
+						minlength: maxLength,
 					});
 				}
 			})
@@ -103,6 +130,63 @@ function makeFields(fieldsArray) {
 	});
 }
 
+// Обработка маски input
+
+function addKeypressMaskListeners() {
+	$("input[mask]")
+		.keydown(function (e) {
+			var key = e.key; //which || e.charCode || e.keyCode || 0;
+			var input = $(this);
+			var mask = $(this).attr("mask");
+			var currentSymbol = input.val().length;
+			// Если в маске не 9 и нажат символ то добавляем в инпут значение из маски
+			if (
+				mask[currentSymbol] != 9 &&
+				key !== "Backspace" &&
+				key !== "Tab" &&
+				key !== "Delete" &&
+				input.val().length < mask.length
+			) {
+				addMaskSymbol(input, mask, currentSymbol);
+			}
+			// Allow numeric (and tab, backspace, delete) keys only
+			return (
+				key == "Backspace" ||
+				key == "Tab" ||
+				key == "Delete" ||
+				(key >= 0 && key <= 9)
+				// key == 8 ||
+				// key == 9 ||
+				// key == 46 ||
+				// (key >= 48 && key <= 57) ||
+				// (key >= 96 && key <= 105)
+			);
+		})
+		.bind("focus click", function () {
+			var input = $(this);
+			var mask = $(this).attr("mask");
+			var currentSymbol = input.val().length;
+
+			if (input.val().length === 0 && mask[currentSymbol] != 9) {
+				addMaskSymbol(input, mask, currentSymbol);
+			} else {
+				var val = input.val();
+				input.val("").val(val); // Ensure cursor remains at the end
+			}
+		});
+
+	// рекурсивно добавляем значения из маски в инпут
+	function addMaskSymbol(input, mask, currentSymbol) {
+		console.log(typeof input.val());
+		var value = mask[currentSymbol];
+		input.val(input.val() + value);
+
+		if (mask[currentSymbol + 1] != 9 && mask[currentSymbol + 1] != null)
+			addMaskSymbol(input, mask, currentSymbol + 1);
+	}
+}
+
+// Заполняет Дропдаун файлами
 function fillDropdown() {
 	fileNames.forEach((name) => {
 		$("#select").append(
@@ -113,6 +197,7 @@ function fillDropdown() {
 	});
 }
 
+// Получают файлы
 function getFileNames() {
 	var fileNames = [];
 	fileNames.push(addpost.name);
